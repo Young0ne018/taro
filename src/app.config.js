4211d301ddaec74880d59bad5f6af22b0f0c805b
yangsijun <yangsijun@youfantech.cn>
2023-12-15 15:33:06 +0800

    init

---
+++ src/app.config.js
@@ -0,0 +1,18 @@
+export default {
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  requiredBackgroundModes: ['location'],
+  permission: {
+    "scope.userLocation": {
+      desc: "你的位置信息将用于小程序位置接口的效果展示" // 高速公路行驶持续后台定位
+    }
+  },
+  requiredPrivateInfos: ['startLocationUpdateBackground','onLocationChange']
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="index">
+    <Counter/>
+  </view>
+</template>
+
+<script setup>
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+import Taro from "@tarojs/taro";
+Taro.startLocationUpdateBackground({
+  complete:(res)=>{
+    console.log(res);
+  }
+})
+Taro.onLocationChange((res)=>{
+  console.log(res);
+})
+</script>
